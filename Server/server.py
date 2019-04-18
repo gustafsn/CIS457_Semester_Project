1386cb39396965270c04e1bf5b51fb83d3100bf3
Nolan G <gustafsn@mail.gvsu.edu>
2019-04-18 17:59:18 -0400

    Initial commit

---
+++ Server/server.py
@@ -0,0 +1,182 @@
+import socket 
+from _thread import *
+import threading 
+import os
+import sys
+from os import listdir
+from os.path import isfile, join
+import json
+
+BUFFER_SIZE = 1024 
+# Projects store all of the projects as a json/dict with their files, users. 
+# Projects should be stored locally on the server as a folder containing the files
+# JSON/DictStructure like:
+# # projects = {
+# 	'proj1' : {
+# 		'users' : ['tim', 'tom', 'toom'],
+# 		'file1' : {
+# 			'isCheckedOut' : False,
+# 			'isModifying' : False,
+# 			'version' : 1
+# 		}
+# 	}
+# # 	<projName2> : {...}
+# # }
+Projects = {}
+
+#print_lock = threading.Lock() 
+
+#The threaded function that will handle most of the parsing
+def threaded(connection): 
+	currentUser = "" #the username for the client.py in this thread
+	selectedProj = "" #project the user wants to do stuff with
+	while True: 
+
+		# data received from client 
+		incMessage = connection.recv(BUFFER_SIZE) 
+		if not incMessage:
+			print('Bye ', connection.getpeername()) 
+			#print_lock.release()
+			break
+			
+		#split the data into a list
+		incStr = incMessage.decode()
+		incList= incStr.split(" ")
+
+		
+		#check which function is requested
+		if incList[0] == "LIST":
+			files = List()
+			filesStr = ",".join(files)
+			connection.send(filesStr.encode())
+		# some semester project stuff in here******************************************************************************************
+		elif incList[0] == "USERNAME":
+			currentUser = incList[1]
+			response = currentUser+" successfully signed in."
+			connection.send(response.encode())
+		elif(incList[0] == "CHECKOUT"):
+			print(incStr)
+			#Select a project. must be a user of the project. update 'selectedProj'
+		elif(incList[0] == "ADDUSER"):
+			print(incStr)
+			#Add a user to a project. Current user must be a part of the project already
+		elif(incList[0] == "CREATE"):
+			print(incStr)
+			#Create a project. on the server side, add the current user to the list 
+			#of users in this project
+		elif(incList[0] == "PULL"):
+			print(incStr)
+			#PULL a file without modifying it (same as RETRIEVE probably)
+		elif(incList[0] == "MODIFY"):
+			print(incStr)
+			#When a user wants to "check out" a file 
+		elif(incList[0] == "PUSH"):
+			print(incStr)
+			#Returning a Checked out file. Only if the current user is the one who 
+			#checked it out with "MODIFY"
+		# ******************************************************************************************
+		elif incList[0] == "RETRIEVE":
+			print('command: RETRIEVE')
+			try:
+				f = open(incList[1], "rb")
+				l = f.read(BUFFER_SIZE)
+				while (l):
+					connection.send(l)
+					l = f.read(BUFFER_SIZE)
+				f.close()
+				print('sent file...')
+			except IOError:
+				noFile = "requested file does not exist..."
+				print(noFile)
+				connection.send(noFile.encode())
+		elif incList[0] == "STORE":
+			print('command: STORE')
+			with open(incList[1], 'wb') as f:
+				print ('file opened')
+				connection.settimeout(2)
+				#endlessly loops on .recv() if there is nothing in the socket, and you don't set a timeout. 
+				#changed back to None after this loop, so other commands don't break. Probably a better way to avoid this problem..
+				while True:
+					print('receiving data...')
+					try:
+						data = connection.recv(BUFFER_SIZE)
+					except socket.timeout:
+						data = "end"
+					if (data == "end"):
+						f.close()
+						print('Successfully got the file')
+						break
+					if (data.decode() == "requested file does not exist..."):
+						print(data.decode())
+						f.close()
+						break
+					# write data to a file
+					f.write(data)
+			f.close()
+			connection.settimeout(None)
+		elif incList[0] == "QUIT":
+			connection.close()
+			break
+		else:
+			connection.send("Invalid Message".encode())
+		
+
+	# connection closed 
+	connection.close()
+
+
+def RetrieveProject():
+	"""Stores the Projects dict on a local file """
+	with open('projects.json') as json_file:  
+		Projects = json.load(json_file)
+	return Projects
+
+
+def StoreProject():
+	"""Retrieves the Projects JSON from a local file """
+	with open('projects.json', 'w') as outfile:  
+		json.dump(Projects, outfile)
+
+def List():
+	files = []
+	root = "."
+	for item in os.listdir(root):
+		if os.path.isfile(os.path.join(root, item)):
+			files.append(item)
+	return files
+
+def Main():
+	# StoreProject()
+	Projects = RetrieveProject()
+	print(Projects)
+	host = "127.0.0.1" 
+
+	# reverse a port on your computer 
+	# in our case it is 8080 but it 
+	# can be anything 
+	port = 8080
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	sock.bind((host, port)) 
+	print("socket binded to post", port) 
+
+	# put the socket into listening mode 
+	sock.listen(5) 
+	print("socket is listening") 
+
+	# a forever loop until client wants to exit 
+	while True: 
+
+		# establish connection with client 
+		connection, addr = sock.accept() 
+
+		# lock acquired by client 
+		#print_lock.acquire() 
+		print('Connected to :', addr[0], ':', addr[1]) 
+
+		# Start a new thread and return its identifier 
+		start_new_thread(threaded, (connection,)) 
+	sock.close() 
+
+
+if __name__ == '__main__': 
+	Main() 
---
+++ client/client.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+
+import socket
+import os
+import sys
+
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+isRunning = True
+isConnected = False
+
+
+def connect_to_server(ip, port):
+    global isConnected
+    global s
+
+    try:
+        s.connect((ip, port))
+        s.settimeout(2)
+        isConnected = True
+    except ConnectionRefusedError:
+        print("failed to establish a connection with: ", ip, "on port : ", port) 
+        isConnected = False
+
+
+def send_request(command):
+    global s
+    args = command.split(" ")
+
+    if(command == "LIST"):
+        s.send(command.encode())
+        data = s.recv(BUFFER_SIZE)
+        files = data.decode().split(",")
+        print(*files, sep='\n')
+        
+    #NEW STUFF FOR SEMESTER PROJECT************************************************
+    elif(args[0] == "USERNAME"):
+        #updates the current user in the threaded connection on the server side
+        s.send(command.encode())
+        data = s.recv(BUFFER_SIZE)
+        print(data.decode())
+    elif(args[0] == "CHECKOUT"):
+        print(command)
+        #Select a project. must be a user of the project
+    elif(args[0] == "ADDUSER"):
+        print(command)
+        #Add a user to a project. Current user must be a part of the project already
+    elif(args[0] == "CREATE"):
+        print(command)
+        #Create a project. on the server side, add the current user to the list 
+        #of users in this project
+    elif(args[0] == "PULL"):
+        print(command)
+        #PULL a file without modifying it (same as RETRIEVE probably)
+    elif(args[0] == "MODIFY"):
+        print(command)
+        #When a user wants to "check out" a file 
+    elif(args[0] == "PUSH"):
+        print(command)
+        #Returning a Checked out file. Only if the current user is the one who 
+        #checked it out with "MODIFY"
+    #NEW STUFF FOR SEMESTER PROJECT************************************************
+    elif(args[0] == "RETRIEVE"):
+        print('command: RETRIEVE')
+        s.send(command.encode())
+        with open(args[1], 'wb') as f:
+            print ('file opened')
+            while True:
+                print('receiving data...')
+                try:
+                    data = s.recv(BUFFER_SIZE)
+                except socket.timeout:
+                    data = "end"
+                if (data == "end"):
+                    f.close()
+                    print('Successfully got the file')
+                    break
+                if (data.decode() == "requested file does not exist..."):
+                    print(data.decode())
+                    f.close()
+                    os.remove(args[1])
+                    break
+                # write data to a file
+                f.write(data)
+        f.close()
+    #STORE probably shouldnt be used
+    elif(args[0] == "STORE"):
+        print('command: STORE')
+        try:
+            f = open(args[1], "rb")
+            s.send(command.encode())
+            l = f.read(BUFFER_SIZE)
+            while (l):
+                s.send(l)
+                l = f.read(BUFFER_SIZE)
+            f.close()
+            print('sent file...')
+        except IOError:
+            noFile = "requested file does not exist..."
+            print(noFile)
+            #s.send(noFile.encode())
+    elif(args[0].upper() == "HELP"):
+        print("\nPossible commands are: \nCONNECT <server name/IP address> <server port>,")
+        print("LIST - to list file on this server")
+        print("RETRIEVE <filename>")
+        print("STORE <filename>")
+        print("QUIT - to close the client")
+
+def main():
+    global s 
+    global isConnected
+    global isRunning
+
+    print("starting client...")
+
+    while(isRunning):
+        if(isConnected):
+            command = input("enter a command: ")
+            send_request(command)
+            
+        else:
+            command = input("enter CONNECT followed by the ip and port in order to connect to the server: ")
+            
+    
+            if (command.split(" ")[0] != "CONNECT"):
+                print("must stablish connection first")
+                continue
+            else:
+                args = command.split(" ")
+                ip = args[1]
+                port = int(args[2])
+                connect_to_server(ip, port)
+                username = input("enter your username (with no spaces): ")
+                username = "USERNAME "+username
+                send_request(username)
+    
+    
+        if(command == "QUIT"):
+            isRunning = False
+    
+    s.close()
+
+
+main()
